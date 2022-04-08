bc69d896f4e365eac3dc97e03f959ab68889c7ac
unknown <zlovred12@gmail.com>
2022-04-08 10:41:10 +0300

    Initial commit

---
+++ src/com/geekbrains/swing/MainClass.java
@@ -0,0 +1,9 @@
+package com.geekbrains.swing;
+
+import com.sun.org.apache.xml.internal.security.utils.JavaUtils;
+
+public class MainClass {
+    public static void main(String[] args) {
+        new XOWindow();
+    }
+}
